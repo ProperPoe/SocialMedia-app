46c3bb430cd1ab90310fd39b025b8c3c0e0b33d6
Steven Trujillo <potencyalmighty@outlook.com>
2023-01-31 21:23:33 -0700

    Creating the beginnings of the Navbar component

---
+++ Client/my-app/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Navbar.scss'
+
+function Navbar() {
+    return (
+        <>
+        <div className='navbar-container'>
+            <div className='logo'>
+                Hiiiiii
+            </div>
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </div>
+        
+        </>
+    )
+}
+
+export default Navbar
