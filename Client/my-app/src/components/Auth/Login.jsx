e769b7fa8613d4d038930eaf568cd45d904bd109
Steven Trujillo <potencyalmighty@outlook.com>
2023-02-01 09:42:28 -0700

    Added styling to the Login and Register form components. The Login and Register components are now inside a card container

--- Client/my-app/src/components/Auth/Login.jsx
+++ Client/my-app/src/components/Auth/Login.jsx
@@ -5,11 +5,20 @@ function Login() {
     return (
         <>
         <div className='form-container'>
-            <form>
-                <input type="text" placeholder='Username'/>
-                <input type="text" placeholder='Password'/>
-                <button>Login</button>
-            </form>
+            <div className='card'>
+                <div className='left'>
+                    <span>Hello</span>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, delectus! Dolorum error quo molestiae recusandae.</p>
+                    <button>Register</button>
+                </div>
+                <div className='right'>
+                    <form>
+                        <input type="text" placeholder='Username'/>
+                        <input type="text" placeholder='Password'/>
+                        <button>Login</button>
+                    </form>
+                </div>
+            </div>
         </div>
         </>
     )
