5e9562b4f096116e4d8c84dd1cdf1809d54cb5e8
Steven Trujillo <potencyalmighty@outlook.com>
2023-02-08 18:34:22 -0700

    Created controllers folder and routes foler to connect with mysql database.

---
+++ Server/controllers/auth.js
@@ -0,0 +1,14 @@
+import { db } from "../connect.js"
+
+export const register = (req, res) => {   
+     const q = "SELECT FROM users WHERE username = ?"
+
+     db.query(q, [req.body.username])
+
+}
+export const login = (req, res) => {    
+
+}
+export const logout = (req, res) => {    
+
+}
---
+++ Server/routes/posts.js
@@ -0,0 +1,8 @@
+import Express from "express";
+import { } from "../controllers/posts.js";
+
+const router = Express.Router()
+
+router.get("")
+
+export default router
