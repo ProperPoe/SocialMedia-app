4ff6734f0e0acc7d880a8baaf4f66f1ecc8a2986
Steven Trujillo <potencyalmighty@outlook.com>
2023-02-16 12:08:35 -0700

    Created an addPosts route in posts.js in Routes folder on server, and created an addPosts controller to the route. Connected this route to the Share.jsx on the client, using React query and Mutations to fetch the posts. Logged in users can now successfully add posts to the mysql database, and the posts are successfully fetched from the mysql database

--- Client/my-app/src/components/Share/Share.jsx
+++ Client/my-app/src/components/Share/Share.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { AuthContext } from '../../context/authContext'
 import './Share.scss'
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { makeRequest } from "../../axios"
 
 function Share() {
+    const [file, setFile] = useState(null);
+    const [desc, setDesc] = useState("")
+    const [upload, setUpload] = useState(false)
+
+    const { currentUser } = useContext(AuthContext);
+
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation((newPost) => {
+        return makeRequest.post("/posts", newPost);
+    }, {
+        onSuccess: () => {
+            // Invalidate and refetch
+            queryClient.invalidateQueries(["posts"])
+        },
+    })
+
+    const handleUploadClick = (e) => {
+        e.preventDefault();
+
+        setUpload(true);
+    }
+    
+    const handleClick = (e) => {
+        e.preventDefault();
+        mutation.mutate({desc})
+    }
     return (
         <>
             <div className="post-form">
                 <form action="">
-                    <textarea type="text" name="" id="" placeholder="Enter a post.." />
-                    <button>Share</button>
+                    <textarea type="text" name="" id="" placeholder="Enter a post.." onChange={(e)=>setDesc(e.target.value)} />
+                    <button className={upload === false ? 'upload' : 'uploadHidden'} onClick={handleUploadClick}>Upload</button>
+                    {upload === true ? <input type="file" id='file' onChange={(e) => setFile(e.target.files[0])} /> : null}
+                    <button onClick={handleClick}>Share</button>
                 </form>
             </div>
         </>
